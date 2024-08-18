f3fc65a12794225b60c887d15033ea5e9cfa318f
FarieB <faraibande@gmail.com>
2024-08-18 19:53:13 +0000

    0x17. Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # create a manifest that fix all termintion of phpp.
 
-exec { 'fix_phpp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+exec { 'fix_wordpress-server-error':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/bin', '/usr/bin/', '/usr/loca/bin/'
 }
