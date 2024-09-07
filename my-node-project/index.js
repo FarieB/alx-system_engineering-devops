fbb03a515a3c5b97123542a72964bcfcd484e2ff
FarieB <faraibande@gmail.com>
2024-09-07 01:25:36 -0700

    public

--- my-node-project/index.js
+++ my-node-project/index.js
@@ -24,3 +24,17 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+/*Serve static files from the 'public' directory*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*Other middleware and routes*/
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
