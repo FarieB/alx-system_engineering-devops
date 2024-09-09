9e63d47fe587507355fd26b1c64991728d7f1daf
FarieB <faraibande@gmail.com>
2024-09-09 14:57:57 -0700

    errors

--- my-node-project/models/User.js
+++ my-node-project/models/User.js
@@ -1,50 +1,50 @@
 'use strict';
 
-const { DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
-const db = require('./index'); // Import the db object from index.js
 
-const User = db.sequelize.define('User', {
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     preferences: {
-        type: DataTypes.JSONB, // Use JSONB to store preferences in a flexible format
-        defaultValue: {} // Default to an empty object
+      type: DataTypes.JSONB, // Use JSONB to store preferences in a flexible format
+      defaultValue: {} // Default to an empty object
     }
-}, {
+  }, {
     hooks: {
-        // This hook will run before creating or updating a user
-        beforeSave: async (user) => {
-            if (user.changed('password')) {
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(user.password, salt); // Hash the password before saving
-            }
+      // This hook will run before creating or updating a user
+      beforeSave: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt); // Hash the password before saving
         }
+      }
     },
     defaultScope: {
-        attributes: { exclude: ['password'] } // Optionally, exclude password by default in queries
+      attributes: { exclude: ['password'] } // Optionally, exclude password by default in queries
     }
-});
+  });
 
-// Method to compare passwords during login
-User.prototype.isValidPassword = async function (password) {
+  // Method to compare passwords during login
+  User.prototype.isValidPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
-};
+  };
 
-module.exports = User;
+  return User;
+};
 
--- my-node-project/models/authMiddleware.js
+++ my-node-project/models/authMiddleware.js
@@ -1,14 +1,17 @@
-require('dotenv').config(); /*Loads environment variables*/
-
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-    const token = req.header('x-auth-token');
+module.exports = function(req, res, next) {
+    // Get token from header
+    const token = req.get('x-auth-token'); // Use req.get instead of req.header
+
+    // Check if no token
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
+
+    // Verify token
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); /*Use the secret from .env*/
+        const decoded = jwt.verify(token, 'yourSecretKey');
         req.user = decoded.user;
         next();
     } catch (err) {
