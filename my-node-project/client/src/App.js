bf771089e09d9d3811af0a7538f66848d37f2653
FarieB <133366471+FarieB@users.noreply.github.com>
2024-09-24 14:29:52 +0200

    Update App.js

--- my-node-project/client/src/App.js
+++ my-node-project/client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UsersList from './components/UsersList'; // Import the UsersList component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Insurance Comparison App</h1>
+      <UsersList /> {/* Use the UsersList component */}
     </div>
   );
-}
+};
 
 export default App;
